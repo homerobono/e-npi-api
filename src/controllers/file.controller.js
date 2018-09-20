7dd2d28512db31314deb9bfd70c72f8243df13b3
Homero Bonomini <homerobono@gmail.com>
2018-09-20 18:28:38 -0300

    Download de pasta via arquivo zip

--- src/controllers/file.controller.js
+++ src/controllers/file.controller.js
@@ -1,10 +1,11 @@
 var mime = require('mime');
 var Promise = require('bluebird');
-var fs = Promise.promisifyAll(require('fs.extra'));
+var fs = Promise.promisifyAll(require('fs-extra'));
 var path = require('path');
 var multer = require('multer');
 var dateformat = require('./utils/dateformat');
 var pathResolver = require('./utils/pathresolver');
+var archiver = require('archiver')
 
 const pathresolver = pathResolver.baseDir = function (req) {
   return global.FILES_DIR;
@@ -19,24 +20,25 @@ var storage = multer.diskStorage({
   }
 });
 
-var upload = multer({ storage: storage }).any();
+var upload = multer({ storage: storage })
 
 exports.upload = function (req, res, next) {
-  console.log(req.body)
-  console.log(req.body.destination)
-  console.log(req.params.body)
-  console.log(req.body.params)
-  console.log(req.destination)
-  upload(req, res, function (err) {
+  upload.any()(req, res, err => {
     if (err) {
-      // An error occurred when uploading
-      console.log(err);
-      return res.status(422).send("an error occured while uploading")
+      err.status(500).send({
+        "result": {
+          "success": false,
+          "error": true
+        }
+      });
     }
-    // No error occured.
-    paths = req.files;
-    next(res.send("Upload completed for " + paths));
-  });
+    res.status(200).send({
+      "result": {
+        "success": true,
+        "error": null
+      }
+    });
+  })
 }
 
 exports.list = function (req, res, next) {
@@ -100,22 +102,55 @@ exports.download = function (req, res, next) {
   var fileName = path.basename(filePath);
   var promise;
 
+  console.log(filePath)
+  console.log(fileName)
+
   promise = fs.statAsync(filePath);
+
+  var mimeType = mime.lookup(filePath);
+  console.log('mimetype: ' + mimeType);
+
+  res.setHeader('Content-disposition', 'attachment; filename=' + fileName);
+  res.setHeader('Content-type', mimeType);
 
   promise = promise.then(function (stat) {
 
     if (!stat.isFile()) {
-      throw new Error("Cannot access file " + filePath + " (or is no file)");
+      console.log('Element is folder: compressing and sending to downloading')
+      var output = fs.createWriteStream('/tmp/' + fileName + '-compressed.zip');
+      var archive = archiver('zip', {
+        zlib: { level: 1 } // Sets the compression level.
+      });
+
+      output.on('close', function () {
+        console.log(archive.pointer() + ' total bytes');
+        console.log('archiver has been finalized and the output file descriptor has closed.');
+      });
+
+      output.on('end', function () {
+        console.log('Data has been drained');
+      });
+
+      archive.on('warning', function (err) {
+        if (err.code === 'ENOENT') {
+          console.log(err)
+        } else throw err;
+      });
+
+      archive.on('error', function (err) {
+        throw err;
+      });
+
+      // pipe archive data to the file
+      archive.pipe(output);
+
+      archive.directory(filePath, false);
+      archive.finalize();
+      archive.pipe(res)
+    } else {
+      var filestream = fs.createReadStream(filePath);
+      filestream.pipe(res);
     }
-
-    var mimeType = mime.lookup(filePath);
-    console.log('mimetype: ' + mimeType);
-
-    res.setHeader('Content-disposition', 'attachment; filename=' + fileName);
-    res.setHeader('Content-type', mimeType);
-
-    var filestream = fs.createReadStream(filePath);
-    filestream.pipe(res);
   });
 
   promise = promise.catch(function (err) {
@@ -131,21 +166,10 @@ exports.download = function (req, res, next) {
   return promise;
 }
 
-exports.uploadResponse = function (req, res, next) {
-
-  res.status(200);
-  res.send({
-    "result": {
-      "success": true,
-      "error": null
-    }
-  });
-}
-
 exports.remove = function (req, res, next) {
 
-  var filePath = path.join(pathResolver.baseDir(req), req.body.params.path);
-  var promise = fs.unlinkAsync(filePath);
+  var filePath = path.join(pathResolver.baseDir(req), req.body.params.path, req.body.params.name);
+  var promise = fs.remove(filePath);
 
   promise = promise.then(function () {
     res.status(200);
@@ -201,10 +225,41 @@ exports.createFolder = function (req, res, next) {
 
 exports.rename = function (req, res, next) {
 
-  var oldPath = path.join(pathResolver.baseDir(req), req.body.params.path);
-  var newPath = path.join(pathResolver.baseDir(req), req.body.params.newPath);
+  var filePath = path.join(pathResolver.baseDir(req), req.body.params.path, req.body.params.name);
+  var newPath = path.join(pathResolver.baseDir(req), req.body.params.path, req.body.params.newName);
 
-  var promise = fs.renameAsync(oldPath, newPath);
+  var promise = fs.renameAsync(filePath, newPath);
+
+  promise = promise.then(function () {
+    res.status(200);
+    res.send({
+      "result": {
+        "success": true,
+        "error": null
+      }
+    });
+  });
+
+  promise = promise.catch(function (err) {
+    res.status(500);
+    res.send({
+      "result": {
+        "success": false,
+        "error": err
+      }
+    });
+  });
+
+  return promise;
+}
+
+exports.move = function (req, res, next) {
+
+  var oldPath = path.join(pathResolver.baseDir(req), req.body.params.path, req.body.params.name);
+  var newPath = path.join(pathResolver.baseDir(req), req.body.params.newPath, req.body.params.name);
+
+  console.log("copying " + oldPath + ' to ' + newPath)
+  var promise = fs.move(oldPath, newPath);
 
   promise = promise.then(function () {
     res.status(200);
@@ -234,7 +289,7 @@ exports.copy = function (req, res, next) {
   var oldPath = path.join(pathResolver.baseDir(req), req.body.params.path);
   var newPath = path.join(pathResolver.baseDir(req), req.body.params.newPath);
 
-  var promise = fs.copyAsync(oldPath, newPath);
+  var promise = fs.copy(oldPath, newPath);
 
   promise = promise.then(function () {
     res.status(200);
--- src/routes/file.route.js
+++ src/routes/file.route.js
@@ -12,10 +12,11 @@ router.get('/files/', authService.authorize, npiController.downloadFiles);
 */
 router.post('/files/list', authService.authorize, fileController.list)
 router.get('/files/download', authService.authorize, fileController.download)
-router.post('/files/upload', authService.authorize, fileController.upload, fileController.uploadResponse)
-router.post('/files/remove', authService.authorize, fileController.upload, fileController.remove)
-router.post('/files/createFolder', authService.authorize, fileController.upload, fileController.createFolder)
+router.post('/files/upload', authService.authorize, fileController.upload)
+router.post('/files/remove', authService.authorize, fileController.remove)
+router.post('/files/createFolder', authService.authorize, fileController.createFolder)
 router.post('/files/rename', authService.authorize, fileController.rename)
+router.post('/files/move', authService.authorize, fileController.move)
 router.post('/files/copy', authService.authorize, fileController.copy)
 
 module.exports = router;
